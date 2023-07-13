1db708756690f083260d1c541a0d38d30224f110
Mitchel <ugwumitchel@gmail.com>
2023-07-13 13:38:55 -0700

    Hello

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main- Entrypoint
+ *
+ * Return: Always 0(success)
+ */
+intmain(void)
+{
+printf(with proper grammar, but the outcome is a piece of art, /n");
+}
+
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ *main- entrypoint
+ *
+ *Return : Always 0 (success)
+ *
+ * int main(void)
+ {
+ printf("Size of a char:%dbyte(s)\n,"size of (char));
+ printf("Size of an int:%dbyte(s)\n",size of (int));
+ printf("Size of a long int:%dbyte(s)n",size of (long int));
+ printf("Size of a long long  int:%dbyte(s)\n," size of (long long int));
+printf("Size of a float :%dbyte(s)\n", size of (float));
+ return(0);
+ }
